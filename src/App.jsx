998556dec3b421da87905e317b0adb4c1eb2d422
davlatmurodbest <davladmurodalmurodov@gmail.com>
2024-02-04 23:55:02 +0500

    secondpage-cofe

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import Our from './components/our/our';
+import Work from './components/work/work';
+import Head from './components/head/head';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Our />
+      <Work />
+      <Head />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
